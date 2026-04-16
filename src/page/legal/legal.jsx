c4cd079fc8184cbeb0fffe3327b4a8649ccc8c4e
NadirYusifov <nadiryusifov448@gmail.com>
2026-04-17 02:45:18 +0400

    add: legal page added in pages folder

---
+++ src/page/legal/legal.jsx
@@ -0,0 +1,103 @@
+import { Link } from "react-router"
+
+const Legal = () => {
+    return (
+        <section>
+            <div className="container mx-auto px-4">
+            <h1 className="mt-3 font-medium text-3xl">Legal</h1>
+                <ul>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+                        <Link className="underline" href="https://tailwindcss.com">https://tailwindcss.com</Link>
+                    </li>
+                    <li>
+                        <p>Formik</p>
+                        <Link className="underline" href="https://formik.org">https://formik.org</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://github.com/bubkoo/html-to-image">html-to-image</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://github.com/raineorshine/npm-check-updates">npm-check-updates</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://react.dev">React</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://react.dev">React DOM</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://github.com/rosskhanas/react-qr-code">react-qr-code</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://tailwindcss.com">Tailwind CSS</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+                    
+                        <Link className="underline" href="https://github.com/jquense/yup">yup</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://eslint.org">@eslint/js</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react">@types/react</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react-dom">@types/react-dom</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://github.com/vitejs/vite-plugin-react/tree/main/packages/plugin-react">@vitejs/plugin-react</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://eslint.org">ESLint</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://react.dev">eslint-plugin-react-hooks</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://github.com/ArnaudBarre/eslint-plugin-react-refresh">eslint-plugin-react-refresh</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://github.com/sindresorhus/globals">globals</Link>
+                    </li>
+                    <li>
+                        <p>@tailwindcss/vite</p>
+
+                        <Link className="underline" href="https://vite.dev">Vite</Link>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Legal
