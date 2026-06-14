77ec68658f8cf77449e69dce89a9ba38e696c524
NadirYusifov <nadiryusifov448@gmail.com>
2026-06-15 01:07:36 +0400

    update: added header component, about page and about page for route and footer component changed imported path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Home } from "./page/home";
 import { Referance } from "./page/referance";
 import { Route, Routes } from "react-router";
-import { Footer } from "./components/layout/footer";
+import { Footer } from "./components/layout/footer/footer";
+import { Header } from "./components/layout/header/header";
+import { About } from "./page/about";
 
 export default function App() {
   return (
     <main>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/referance" element={<Referance />} />
       </Routes>
       <Footer />
