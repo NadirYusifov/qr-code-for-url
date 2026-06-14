2d54e981ae0e97253a62b7204910660d858010aa
NadirYusifov <nadiryusifov448@gmail.com>
2026-06-15 01:11:07 +0400

    formating: referance page clean formating

--- src/page/referance/index.jsx
+++ src/page/referance/index.jsx
@@ -1,107 +1,179 @@
 export const Referance = () => {
-    return (
-        <section className="mb-30">
-            <div className="container mx-auto px-4">
-                <h1 className="mt-3 font-medium text-3xl">Referances</h1>
-                <div className="mt-7">
-                    <h2 className="font-bold text-2xl">Packages:</h2>
-                    <ul>
-                        <li>
-                            <p>@tailwindcss/vite</p>
-                            <a className="underline" href="https://tailwindcss.com">https://tailwindcss.com</a>
-                        </li>
-                        <li>
-                            <p>Formik</p>
-                            <a className="underline" href="https://formik.org">https://formik.org</a>
-                        </li>
-                        <li>
-                            <p>html-to-image</p>
-                            <a className="underline" href="https://github.com/bubkoo/html-to-image">https://github.com/bubkoo/html-to-image</a>
-                        </li>
-                        <li>
-                            <p>npm-check-updates</p>
-                            <a className="underline" href="https://github.com/raineorshine/npm-check-updates">https://github.com/raineorshine/npm-check-updates</a>
-                        </li>
-                        <li>
-                            <p>React</p>
-                            <a className="underline" href="https://react.dev">https://react.dev</a>
-                        </li>
-                        <li>
-                            <p>React DOM</p>
-                            <a className="underline" href="https://react.dev">https://react.dev</a>
-                        </li>
-                        <li>
-                            <p>react-qr-code</p>
-                            <a className="underline" href="https://github.com/rosskhanas/react-qr-code">https://github.com/rosskhanas/react-qr-code</a>
-                        </li>
-                        <li>
-                            <p>Tailwind CSS</p>
-                            <a className="underline" href="https://tailwindcss.com">https://tailwindcss.com</a>
-                        </li>
-                        <li>
-                            <p>yup</p>
-                            <a className="underline" href="https://github.com/jquense/yup">https://github.com/jquense/yup</a>
-                        </li>
-                        <li>
-                            <p>@eslint/js</p>
+  return (
+    <section className="mb-30">
+      <div className="container mx-auto px-4">
+        <h1 className="mt-3 font-medium text-3xl">Referances</h1>
+        <div className="mt-7">
+          <h2 className="font-bold text-2xl">Packages:</h2>
+          <ul>
+            <li>
+              <p>@tailwindcss/vite</p>
+              <a className="underline" href="https://tailwindcss.com">
+                https://tailwindcss.com
+              </a>
+            </li>
+            <li>
+              <p>Formik</p>
+              <a className="underline" href="https://formik.org">
+                https://formik.org
+              </a>
+            </li>
+            <li>
+              <p>html-to-image</p>
+              <a
+                className="underline"
+                href="https://github.com/bubkoo/html-to-image"
+              >
+                https://github.com/bubkoo/html-to-image
+              </a>
+            </li>
+            <li>
+              <p>npm-check-updates</p>
+              <a
+                className="underline"
+                href="https://github.com/raineorshine/npm-check-updates"
+              >
+                https://github.com/raineorshine/npm-check-updates
+              </a>
+            </li>
+            <li>
+              <p>React</p>
+              <a className="underline" href="https://react.dev">
+                https://react.dev
+              </a>
+            </li>
+            <li>
+              <p>React DOM</p>
+              <a className="underline" href="https://react.dev">
+                https://react.dev
+              </a>
+            </li>
+            <li>
+              <p>react-qr-code</p>
+              <a
+                className="underline"
+                href="https://github.com/rosskhanas/react-qr-code"
+              >
+                https://github.com/rosskhanas/react-qr-code
+              </a>
+            </li>
+            <li>
+              <p>Tailwind CSS</p>
+              <a className="underline" href="https://tailwindcss.com">
+                https://tailwindcss.com
+              </a>
+            </li>
+            <li>
+              <p>yup</p>
+              <a className="underline" href="https://github.com/jquense/yup">
+                https://github.com/jquense/yup
+              </a>
+            </li>
+            <li>
+              <p>@eslint/js</p>
 
-                            <a className="underline" href="https://eslint.org">https://eslint.org</a>
-                        </li>
-                        <li>
-                            <p>@types/react</p>
-                            <a className="underline" href="https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react">https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react</a>
-                        </li>
-                        <li>
-                            <p>@types/react-dom</p>
-                            <a className="underline" href="https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react-dom">https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react-dom</a>
-                        </li>
-                        <li>
-                            <p>@vitejs/plugin-react</p>
-                            <a className="underline" href="https://github.com/vitejs/vite-plugin-react/tree/main/packages/plugin-react">https://github.com/vitejs/vite-plugin-react/tree/main/packages/plugin-react</a>
-                        </li>
-                        <li>
-                            <p>ESLint</p>
-                            <a className="underline" href="https://eslint.org">https://eslint.org</a>
-                        </li>
-                        <li>
-                            <p>eslint-plugin-react-hooks</p>
-                            <a className="underline" href="https://react.dev">https://react.dev</a>
-                        </li>
-                        <li>
-                            <p>eslint-plugin-react-refresh</p>
-                            <a className="underline" href="https://github.com/ArnaudBarre/eslint-plugin-react-refresh">https://github.com/ArnaudBarre/eslint-plugin-react-refresh</a>
-                        </li>
-                        <li>
-                            <p>globals</p>
-                            <a className="underline" href="https://github.com/sindresorhus/globals">https://github.com/sindresorhus/globals</a>
-                        </li>
-                        <li>
-                            <p>Vite</p>
-                            <a className="underline" href="https://vite.dev">https://vite.dev</a>
-                        </li>
-                    </ul>
-                </div>
+              <a className="underline" href="https://eslint.org">
+                https://eslint.org
+              </a>
+            </li>
+            <li>
+              <p>@types/react</p>
+              <a
+                className="underline"
+                href="https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react"
+              >
+                https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react
+              </a>
+            </li>
+            <li>
+              <p>@types/react-dom</p>
+              <a
+                className="underline"
+                href="https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react-dom"
+              >
+                https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react-dom
+              </a>
+            </li>
+            <li>
+              <p>@vitejs/plugin-react</p>
+              <a
+                className="underline"
+                href="https://github.com/vitejs/vite-plugin-react/tree/main/packages/plugin-react"
+              >
+                https://github.com/vitejs/vite-plugin-react/tree/main/packages/plugin-react
+              </a>
+            </li>
+            <li>
+              <p>ESLint</p>
+              <a className="underline" href="https://eslint.org">
+                https://eslint.org
+              </a>
+            </li>
+            <li>
+              <p>eslint-plugin-react-hooks</p>
+              <a className="underline" href="https://react.dev">
+                https://react.dev
+              </a>
+            </li>
+            <li>
+              <p>eslint-plugin-react-refresh</p>
+              <a
+                className="underline"
+                href="https://github.com/ArnaudBarre/eslint-plugin-react-refresh"
+              >
+                https://github.com/ArnaudBarre/eslint-plugin-react-refresh
+              </a>
+            </li>
+            <li>
+              <p>globals</p>
+              <a
+                className="underline"
+                href="https://github.com/sindresorhus/globals"
+              >
+                https://github.com/sindresorhus/globals
+              </a>
+            </li>
+            <li>
+              <p>Vite</p>
+              <a className="underline" href="https://vite.dev">
+                https://vite.dev
+              </a>
+            </li>
+          </ul>
+        </div>
 
-                <div>
-                    <h2 className="font-bold text-2xl">Special Thanks:</h2>
-                    <ul>
-                        <li>
-                            <p>The ERIN</p>
-                            <div className="flex gap-1">
-                                <p>GitHub: Erinle Feyijimi</p>
-                                <a className="underline" href="https://github.com/Jhimmyofficial">[https://github.com/Jhimmyofficial]</a>
-                            </div>
-                            <div>
-                                <p>DEV: The ERIN, Creating Dynamic QR Codes Using React.js: A Step-by-Step Tutorial</p>
-                                <a href="https://dev.to/onlyoneerin/creating-dynamic-qr-codes-using-reactjs-a-step-by-step-tutorial-341a" className="underline">[https://dev.to/onlyoneerin/creating-dynamic-qr-codes-using-reactjs-a-step-by-step-tutorial-341a]</a>
-                            </div>
-                        </li>
-                    </ul>
+        <div>
+          <h2 className="font-bold text-2xl">Special Thanks:</h2>
+          <ul>
+            <li>
+              <p>The ERIN</p>
+              <div className="flex gap-1">
+                <p>GitHub: Erinle Feyijimi</p>
+                <a
+                  className="underline"
+                  href="https://github.com/Jhimmyofficial"
+                >
+                  [https://github.com/Jhimmyofficial]
+                </a>
+              </div>
+              <div>
+                <p>
+                  DEV: The ERIN, Creating Dynamic QR Codes Using React.js: A
+                  Step-by-Step Tutorial
+                </p>
+                <a
+                  href="https://dev.to/onlyoneerin/creating-dynamic-qr-codes-using-reactjs-a-step-by-step-tutorial-341a"
+                  className="underline"
+                >
+                  [https://dev.to/onlyoneerin/creating-dynamic-qr-codes-using-reactjs-a-step-by-step-tutorial-341a]
+                </a>
+              </div>
+            </li>
+          </ul>
 
-                    <p className="text-xl font-bold">This is project Open Source.</p>
-                </div>
-            </div>
-        </section>
-    )
-}
-
+          <p className="text-xl font-bold">This is project Open Source.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
